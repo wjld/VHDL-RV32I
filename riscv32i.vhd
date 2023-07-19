543349e56eda9d57560eccfd521cb503d63f4b90
wjld <98484634+wjld@users.noreply.github.com>
2023-07-19 16:04:00 -0300

    add instruction fetch pipeline stage

--- riscv32i.vhd
+++ riscv32i.vhd
@@ -11,5 +11,30 @@ entity riscv32i is port(
 end;
 
 architecture arch of riscv32i is
+    signal brJmpS, stallS : std_logic;
+    signal pc, plus4PC, brJmpPC, pcS : std_logic_vector(31 downto 0);
+    signal instrS, ifidS, ifidFlushS: std_logic_vector(31 downto 0);
 begin
+    ----------------------- instruction fetch
+    muxPC: entity work.mux2(arch) port map(
+        a0 => plus4PC, a1 => brJmpPC, sel => brJmpS, b => pcS
+    );
+    process(clk) is begin
+        if(clk'event and clk = '1') then
+            if(stallS = '1') then
+                pc <= pcS;
+            end if;
+        end if;
+    end process;
+    pcPlus4Adder: entity work.adder(arch) port map(
+        a => pc, b => x"00000004", c => plus4PC
+    );
+    instrMem: entity work.memory(arch) port map(
+        clk => clk, wren => memIWr, rden => '1', inAddr => memIAddr,
+        outAddr => pc(9 downto 0), inData => memIData, outData => instrS
+    );
+    IFID: entity work.pipelineReg(arch) port map(
+        clk => clk, wren => stallS, rst => ifidFlushS,
+        regIn => pc & instrS, regOut => ifidS
+    );
 end;
