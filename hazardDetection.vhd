c15f6cba254c560c8b5a68140684c483d9f1c5d9
wjld <98484634+wjld@users.noreply.github.com>
2023-07-19 09:51:58 -0300

    add hazard detection module

---
+++ hazardDetection.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity hazardDetection is port(
+    rdMem, beq : in std_logic;
+    rd, rs1, rs2 : in std_logic_vector(12 downto 0);
+    funct3Opcode : in std_logic_vector(9 downto 0);
+    stall, flush : out std_logic
+);
+end;
+
+architecture arch of hazardDetection is
+begin
+    stall <= '1' when rdMem = '1' and (rd = rs1 or rd = rs2) else '0';
+    flush <= beq when funct3Opcode = "0001100011" else '0';
+end;
