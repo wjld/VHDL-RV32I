9ff230370fc9f33a0889bb6d264b3b75cde34cb4
wjld <98484634+wjld@users.noreply.github.com>
2023-07-21 16:18:28 -0300

    fix branches and jumps execution

--- riscv32i.vhd
+++ riscv32i.vhd
@@ -11,26 +11,23 @@ entity riscv32i is port(
 end;
 
 architecture arch of riscv32i is
-    function to_logic(bool : boolean) return std_logic is begin
-        if bool then return '1';
-        else return '0'; end if;
-    end function;
-
     signal brJmpS, stallS : std_logic;
     signal pc, plus4PC, brJmpPC, pcS : std_logic_vector(31 downto 0);
     signal instrS : std_logic_vector(31 downto 0);
     signal ifidS : std_logic_vector(41 downto 0);
 
     signal aluSrcS, cntrlBrS, memRdS, memWrS : std_logic;
-    signal regWrS, mem2RegS, auipcS, luiS : std_logic;
+    signal regWrS, mem2RegS, auipcS, luiS, jalS, jalrS, beqS : std_logic;
     signal hazardFlushS, ifidFlushS, idexFlushS, exmemFlushS : std_logic;
     signal aluOpS : std_logic_vector(3 downto 0);
-    signal immS, beqJalPC, reg1S, reg2S : std_logic_vector(31 downto 0);
-    signal idexS : std_logic_vector(129 downto 0);
+    signal immS, beqPC, reg1S, reg2S : std_logic_vector(31 downto 0);
+    signal idexS : std_logic_vector(134 downto 0);
 
-    signal fwdRs1S, fwdRs2S, aluA, aluB, aluOutS : std_logic_vector(31 downto 0);
+    signal branchJalPC, jalrPC, pcAddrS : std_logic_vector(31 downto 0);
+    signal fwdRs1S, fwdRs2S : std_logic_vector(31 downto 0);
+    signal aluA, aluB, aluOutS : std_logic_vector(31 downto 0);
     signal forwardAS, forwardBS : std_logic_vector(1 downto 0);
-    signal exmemS : std_logic_vector(72 downto 0);
+    signal exmemS : std_logic_vector(106 downto 0);
 
     signal dataMemOutS, dataAddrS, dataDataS : std_logic_vector(31 downto 0);
     signal memwbS : std_logic_vector(70 downto 0);
@@ -38,9 +35,19 @@ architecture arch of riscv32i is
     signal wbS : std_logic_vector(31 downto 0);
 begin
     ----------------------- instruction fetch
-    muxPC: entity work.mux2(arch) port map(
-        a0 => plus4PC, a1 => brJmpPC, sel => brJmpS, b => pcS
+    muxBeqOthers: entity work.mux2(arch) port map(
+        a0 => beqPC, a1 => exmemS(104 downto 73),
+        sel => (not hazardFlushS) and brJmpS,
+        b => brJmpPC
     );
+    muxPC: entity work.mux2(arch) port map(
+        a0 => plus4PC, a1 => brJmpPC,
+        sel => hazardFlushS or brJmpS,
+        b => pcS
+    );
+    ifidFlushS <= hazardFlushS or brJmpS;
+    idexFlushS <= brJmpS;
+    exmemFlushS <= brJmpS;
     process(clk) is begin
         if(clk'event and clk = '1') then
             if(stallS = '1') then
@@ -64,10 +71,12 @@ begin
         opcode => ifidS(6 downto 2), funct3 => ifidS(14 downto 12),
         funct7 => ifidS(30), aluOp => aluOpS, aluSrc => aluSrcS,
         branch => cntrlBrS, memRd => memRdS, memWr => memWrS,
-        regWr => regWrS, mem2Reg => mem2RegS, auipc => auipcS, lui => luiS
+        regWr => regWrS, mem2Reg => mem2RegS, auipc => auipcS,
+        lui => luiS, jal => jalS, jalr => jalRS
     );
+    beqS <= '1' when reg1S = reg2S else '0';
     hazardDetectionUnit: entity work.hazardDetection port map(
-        rdMem => idexS(67), beq => to_logic(reg1S = reg2S),
+        rdMem => idexS(67), beq => beqS,
         rd => memwbS(4 downto 0), rs1 => ifidS(19 downto 15),
         rs2 => ifidS(24 downto 20),
         funct3Opcode => ifidS(14 downto 12) & ifidS(6 downto 0),
@@ -76,24 +85,28 @@ begin
     immGen: entity work.genImm32(arch) port map(
         instr => ifidS(31 downto 0), imm32 => immS
     );
-    beqJalAdder: entity work.adder(arch) port map(
+    beqAdder: entity work.adder(arch) port map(
         a => x"00000" & "00" & ifidS(41 downto 32),
-        b => immS, c => beqJalPC
+        b => immS, c => beqPC
     );
     regFile: entity work.xregs(arch) port map(
         clk => clk, wren => memwbS(69), rs1 => ifidS(19 downto 15),
         rs2 => ifidS(24 downto 20), rd => memwbS(4 downto 0),
         data => wbS, ro1 => reg1S, ro2 => reg2S
     );
-    IDEX: entity work.pipelineReg(arch) generic map(88) port map(
+    IDEX: entity work.pipelineReg(arch) generic map(93) port map(
         clk => clk, wren => '1', rst => idexFlushS,
-        regIn => luiS & auipcS & mem2RegS & regWrS & memWrS & memRdS
-               & cntrlBrS & aluOpS & aluSrcS & ifidS(41 downto 32) & reg1S
-               & reg2S & immS & ifidS(19 downto 15) & ifidS(24 downto 20)
-               & ifidS(11 downto 7),
+        regIn => cntrlBrS & jalrS & jalS & luiS & auipcS & mem2RegS & regWrS 
+               & memWrS & memRdS & aluOpS & aluSrcS & ifidS(41 downto 32)
+               & reg1S & reg2S & immS & ifidS(19 downto 15)
+               & ifidS(24 downto 20) & ifidS(11 downto 7),
         regOut => idexS
     );
     ----------------------- instruction execute
+    branchJalAdder: entity work.adder(arch) port map(
+        a => x"00000" & "00" & idexS(120 downto 111),
+        b => idexS(46 downto 15), c => branchJalPC
+    );
     forwardingUnit: entity work.forwarding(arch) port map(
         exMemRegWr => exmemS(71), memWbRegWr => memwbS(69),
         rs1 => idexS(14 downto 10), rs2 => idexS(9 downto 5),
@@ -109,24 +122,34 @@ begin
         sel => forwardBS, b => fwdRs2S
     );
     alu1Mux: entity work.mux3(arch) port map(
-        a0 => fwdRs1S,
-        a1 => x"00000" & "00" & idexS(120 downto 111),
-        a2 => x"00000000", sel => idexS(129 downto 128), b => aluA
+        a0 => fwdRs1S, a1 => x"00000" & "00" & idexS(120 downto 111),
+        a2 => x"00000000", sel => (or idexS(133 downto 131)) & idexS(130),
+        b => aluA
     );
-    alu2Mux: entity work.mux2(arch) port map(
-        a0 => fwdRs1S, a1 => idexS(46 downto 15),
-        sel => idexS(121), b => aluB
+    jalrAdder: entity work.adder(arch) port map(
+        a => fwdRs1S, b => idexS(46 downto 15), c => jalrPC
+    );
+    pcAddrMux: entity work.mux2(arch) port map(
+        a0 => branchJalPC, a1 => jalrPC(31 downto 1) & '0',
+        sel => jalrS, b => pcAddrS
+    );
+    alu2Mux: entity work.mux3(arch) port map(
+        a0 => fwdRs2S, a1 => idexS(46 downto 15),
+        a2 => x"00000" & "00" & ifidS(41 downto 32),
+        sel => (idexS(133) or idexS(132))
+             & (idexS(121) and (not (idexS(133) or idexS(132)))), b => aluB
     );
     alu: entity work.alu(arch) port map(
-        opcode => idexS(125 downto 122), A => aluA, B => aluB,
-        Z => aluOutS
+        opcode => idexS(125 downto 122), A => aluA, B => aluB, Z => aluOutS
     );
-    EXMEM: entity work.pipelineReg(arch) generic map(31) port map(
+    EXMEM: entity work.pipelineReg(arch) generic map(65) port map(
         clk => clk, wren => '1', rst => exmemFlushS,
-        regIn => idexS(127 downto 124) & aluOutS & aluB & idexS(4 downto 0),
+        regIn => idexS(134) & (or idexS(133 downto 132)) & pcAddrS
+               & idexS(129 downto 126) & aluOutS & aluB & idexS(4 downto 0),
         regOut => exmemS
     );
     ----------------------- memory access
+    brJmpS <= (exmemS(106) and exmemS(37)) or exmemS(105);
     dataMemAddrMux: entity work.mux2(arch) port map(
         a0 => exmemS(68 downto 37), a1 => x"00000" & "00" & memDAddr,
         sel => memDWr, b => dataAddrS
